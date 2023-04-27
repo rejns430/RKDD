cad8c0c9d31cf6267311e249c7a4d5c058f92b75
rejns430 <reinis.net123@gmail.com>
2023-04-27 13:04:55 +0300

    izmainas

--- katalogs.php
+++ katalogs.php
@@ -19,6 +19,9 @@ $result7 = mysqli_query($savienojums, $join);
 <html lang="en">
 
 <head>
+    <script>
+        
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
